ea638fad07d65af1cecdec9d698ef5a79595a002
NoahFola <noahfola2005@gmail.com>
2025-06-26 13:23:52 +0100

    new commit

---
+++ AVLBinaryTree/BinaryNode.java
@@ -0,0 +1,12 @@
+package AVLBinaryTree;
+
+public class BinaryNode {
+    public int value;
+    public int height;
+    public BinaryNode left;
+    public BinaryNode right;
+
+    BinaryNode() {
+        this.height = 0;
+    }
+}
